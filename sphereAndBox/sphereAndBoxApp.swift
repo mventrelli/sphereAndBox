2222e3f040008566b12fa10a28e487c5ed1a25c8
Matt Ventrelli <mventrelli@hawk.iit.edu>
2023-01-13 16:47:33 -0600

    Initial Commit

---
+++ sphereAndBox/sphereAndBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  sphereAndBoxApp.swift
+//  sphereAndBox
+//
+//  Created by IIT PHYS 440 on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct sphereAndBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
